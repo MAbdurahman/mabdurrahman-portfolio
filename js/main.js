757722ab579878adfdede3b1a72509bb1839a193
MAbdurahman <mdbdrrhm@gmail.com>
2024-02-09 09:24:52 -0600

    Add self portrait image, adjust colors, and add preloader

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/********* maabdurrahman-portfolio-scripts **********/
+
+/*===================================================
+          preloader
+=====================================================*/
+$(window).on('load', function () {
+   // makes sure that whole site is loaded
+   $('#preloader__gif, #preloader').fadeOut(5000, function () {});
+
+});
